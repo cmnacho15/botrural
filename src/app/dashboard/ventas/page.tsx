5544e9ba64f0caf372804b03eccf1c8caa96b099
Macboor Air <macboorair@192.168.1.110>
2025-12-02 15:24:12 -0300

    actualización de código

--- src/app/dashboard/ventas/page.tsx
+++ src/app/dashboard/ventas/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-export const dynamic = "force-dynamic"
-
 import { useState, useMemo } from 'react'
 import useSWR from 'swr'
 import ModalVenta from '@/app/components/modales/ModalVenta'
